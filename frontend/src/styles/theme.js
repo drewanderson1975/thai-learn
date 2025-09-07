c1fb734543da2e421f3a93684cc32620795cfbfa
drewanderson1975 <drew.anderson@ymail.com>
2025-09-07 14:38:46 +0100

    Add theme.js with styles and helpers

---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,12 @@
+// theme.js
+
+const toneClassStyles = { /* your styles here */ };
+const reviewStatusStyles = { /* your styles here */ };
+const intentStyles = { /* your styles here */ };
+const radii = { /* your styles here */ };
+const focusRing = { /* your styles here */ };
+
+function getToneClassStyle() { /* your code here */ }
+function getReviewStatusStyle() { /* your code here */ }
+
+export { toneClassStyles, reviewStatusStyles, intentStyles, radii, focusRing, getToneClassStyle, getReviewStatusStyle };
